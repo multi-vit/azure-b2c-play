804a4e5ab096ad94c17a99c1692473f8a150abe6
multi-vit <103585624+multi-vit@users.noreply.github.com>
2024-09-16 14:15:48 +0100

    Use middleware to protect all routes

---
+++ next-azure/src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import withAuth from "next-auth/middleware";
+
+export default withAuth(function middleware() {
+  const response = NextResponse.next();
+  return response;
+});
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
